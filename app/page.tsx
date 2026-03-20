4440967522b2a03c942bf1bb2a9231b53c960d4c
Harshal Margaj <harshal.margaj12@gmail.com>
2026-03-20 21:54:53 +0530

    fix: design

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
 	);
 
 	return (
-		<div className="bg-white h-full">
+		<div className="mx-auto max-w-6xl px-4 py-6 sm:px-6 lg:px-8 lg:py-10">
 			<AppHeader />
 
 			<QuickSelectBar isLoading={isLoading} onSelect={applyPreset} />
